30184d4a8e7dc820f09b1db3c25fa81a5740be70
delagardeEsir <78495424+delagardeEsir@users.noreply.github.com>
2021-02-05 17:17:43 +0100

    Update App.java

--- tuto_maven/src/main/java/mv/tuto_maven/App.java
+++ tuto_maven/src/main/java/mv/tuto_maven/App.java
@@ -1,11 +1,5 @@
 package mv.tuto_maven;
 
-import java.util.LinkedList;
-
-import com.itextpdf.text.List;
-
-import mv.tuto_maven.FirstPdf;
-
 /**
  * Hello world!
  *
@@ -14,23 +8,7 @@ public class App {
     public static void main(String[] args )
     {
         System.out.println( "Hello World!" );
-        
-        int f = 12;
-        f = 12;
-        f = 12;
-        int i = useless(f);
-        double y = Math.sqrt(f);
-        f = 12;
-        
         //on va enlever le code mort !
     }
-    
-    public static int useless(int f) {
-    	double PI = 3.141592;
-    	LinkedList<Double> listeInutile = new LinkedList<Double>();
-    	listeInutile.add(PI);
-    	listeInutile.add(PI);
-    	listeInutile.remove();
-    	return f;
-    }
+//on a enlevé le code mort !
 }
